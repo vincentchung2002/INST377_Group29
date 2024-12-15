7a5619d741986aa76131de6c7d3ccdebd1beba89
Gene <gene.y.ziel@gmail.com>
2024-12-15 14:57:06 -0500

    Added Supabase key. Previously the key wasn't added correctly

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
 
 const supabaseUrl = "https://fbqdpuemmxhtrauhodec.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZicWRwdWVtbXhodHJhdWhvZGVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQyODQzNDMsImV4cCI6MjA0OTg2MDM0M30.pUD3sqfRkxbFuspCwfsGS7SspjcmDlj0EAVbDHyh8yY;
 const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey);
 
 app.get("/api/bikes/networks/default", (req, res) => {
